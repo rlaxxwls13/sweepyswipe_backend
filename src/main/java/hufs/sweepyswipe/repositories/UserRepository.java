17946aea8b0e367a4c561f709ca9e2251d673639
hajin <kimhajin01@naver.com>
2024-06-12 22:21:30 +0900

    delete folder

--- src/main/java/hufs/sweepyswipe/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package hufs.sweepyswipe.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUserId(String userId);
-}
